d01ba80c35ceea1710e4587687cca22e08430a73
root <root@lvps84-39-96-88.my-simplyroot.de>
2017-08-15 09:29:30 +0200

    2017-08-15

--- backup_script.sh
+++ backup_script.sh
@@ -4,13 +4,14 @@
 TARGET=/home/andre/git/nfltipappdb_backup
 DB=nfltipappdb
 CONF=/etc/mysql/debian.cnf
-if [ ! -r $CONF ]; then /usr/bin/logger "$0 - auf $CONF konnte nicht zugegriffen werden"; exit 1; fi
-if [ ! -d $TARGET ] || [ ! -w $TARGET ]; then /usr/bin/logger "$0 - Backup-Verzeichnis nicht beschreibbar"; exit 1; fi
+if [ ! -r $CONF ]; then echo "Auf $CONF konnte nicht zugegriffen werden" >> /var/log/backuplog; exit 1; fi
+if [ ! -d $TARGET ] || [ ! -w $TARGET ]; then echo "Backup-Verzeichnis nicht beschreibbar" >> /var/log/backuplog; exit 1; fi
 
 NOW=$(date +"%Y-%m-%d")
 
 /usr/bin/mysqldump --defaults-extra-file=$CONF --skip-extended-insert --skip-comments $DB > $TARGET/$DB.sql
 
+echo "Backup created, now uploading to git..." >> /var/log/backuplog
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
 cd $TARGET
@@ -18,7 +19,7 @@ git add .
 git commit -m "$NOW"
 git push --verbose
 
-/usr/bin/logger "$0 - Backup von $NOW erfolgreich durchgefuehrt"
+/usr/bin/logger "Backup von $NOW erfolgreich durchgefuehrt"
 exit 0
 
 #zur wiederherstellung folgender befehl:
